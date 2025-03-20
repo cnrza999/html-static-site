f4cd058f6f8c38cd97430f2ed8d428ba9c4477e9
cnrza999 <cnrza6011064@gmail.com>
2025-03-20 21:15:57 +0700

    fixed error

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -60,28 +60,28 @@ class TestTextNode(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
-        self.assertEqual(html_node.props, None)
+        self.assertEqual(html_node.props, {})
 
     def test_bold(self):
         node = TextNode("This is bold text", TextType.BOLD)
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "b")
         self.assertEqual(html_node.value, "This is bold text")
-        self.assertEqual(html_node.props, None)
+        self.assertEqual(html_node.props, {})
 
     def test_italic(self):
         node = TextNode("This is italic text", TextType.ITALIC)
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "i")
         self.assertEqual(html_node.value, "This is italic text")
-        self.assertEqual(html_node.props, None)
+        self.assertEqual(html_node.props, {})
 
     def test_code(self):
         node = TextNode("def foo(): pass", TextType.CODE)
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "code")
         self.assertEqual(html_node.value, "def foo(): pass")
-        self.assertEqual(html_node.props, None)
+        self.assertEqual(html_node.props, {})
 
     def test_link(self):
         node = TextNode("Click here", TextType.LINK, url="https://example.com")
